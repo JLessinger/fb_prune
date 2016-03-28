ea4627e6bb370e6f776b11e32609eb75ea9415f1
Jonathan Lessinger <jonathan.lessinger@gmail.com>
2016-03-28 02:02:26 -0400

    methods to build object structure; no facepy calls yet

--- fb-prune.py
+++ fb-prune.py
@@ -12,7 +12,7 @@ class UserInfo:
         self.dirty_graph = DirtyGraphAPI(self.access_token)
 
     def as_json_obj(self, max_depth):
-        return self.dirty_graph.get_all('me/', max_depth)
+        return self.dirty_graph.get_all_obj_data('me/', max_depth)
 
     def __str__(self):
         return json.dumps(self.as_json_obj(3), indent=2, sort_keys=True)
@@ -22,10 +22,39 @@ class DirtyGraphAPI():
     def __init__(self, access_token):
         self.graph = GraphAPI(access_token)
 
-    def get_all(self, path, max_search_depth):
-        json_str = '{"__complex__": true, "real": 1, "imag": 2}'
-        parsed = json.loads(json_str)
-        return parsed
+    def get_fields_for_obj(self, path):
+        pass
+
+    def get_connection_types(self, path):
+        pass
+
+    def get_connection_path(self, obj_path, con_type):
+        pass
+
+    def get_all_data_ids(self, path):
+        pass
+        # do paging
+
+    # expects an endpoint that represents a list
+    def get_all_list_data(self, path, max_search_depth):
+        if max_search_depth == 0:
+            return ['[MAX DEPTH REACHED]']
+        else:
+            pass
+            # construct list via paging
+            return [self.get_all_obj_data(self.path_of(data_id), max_search_depth - 1) \
+                    for data_id in self.get_all_data_ids(path)]
+
+    # expects an endpoint that returns a single node (json object)
+    def get_all_obj_data(self, path, max_search_depth):
+        if max_search_depth == 0:
+            return {'[MAX DEPTH REACHED' : '...'}
+        else:
+            fields = self.get_fields_for_obj(path)
+            connection_types = self.get_connection_types(path)
+            connections = {type : self.get_all_list_data(self.get_connection_path(path, type), max_search_depth - 1) \
+                           for type in connection_types}
+            return {'fields' : fields, 'connections' : connections}
         
         
 ## API usage:
