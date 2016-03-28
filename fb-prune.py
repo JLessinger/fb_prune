ca705ee7db33cdfe5c00e81144f8a711e0eeca09
Jonathan Lessinger <jonathan.lessinger@gmail.com>
2016-03-28 01:13:58 -0400

    ignore .idea, add user json -> string API

--- fb-prune.py
+++ fb-prune.py
@@ -1,25 +1,35 @@
 from facepy import *
+import json
 
 """
 Wraps facepy
 """
 
 class UserInfo:
-    def __init__(self):
-        pass
+    def __init__(self, uid, access_token):
+        self.id = uid
+        self.access_token = access_token
+        self.dirty_graph = DirtyGraphAPI(self.access_token)
+
+    def as_json_obj(self, max_depth):
+        return self.dirty_graph.get_all('me/', max_depth)
 
     def __str__(self):
-        return "this is going to contain all the text related to the user. search it."
+        return json.dumps(self.as_json_obj(3), indent=2, sort_keys=True)
         
 
 class DirtyGraphAPI():
     def __init__(self, access_token):
         self.graph = GraphAPI(access_token)
+
+    def get_all(self, path, max_search_depth):
+        json_str = '{"__complex__": true, "real": 1, "imag": 2}'
+        parsed = json.loads(json_str)
+        return parsed
         
         
 ## API usage:
 if __name__ == '__main__':
-    test_user = UserInfo()
-    mystring = str(test_user) # search this
-    print mystring 
-    
+    user = UserInfo(100, 'abc')
+    print str(user)
+
