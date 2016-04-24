ee217bbe24f94f5d7bcdcfb91c4fec844515b153
Aidan Fitzgerald <duckjr159@gmail.com>
2016-04-23 23:01:25 -0400

    creating login mockup file -- not sure how this works

---
+++ login_mockup.py
@@ -0,0 +1,9 @@
+from getpass import getpass
+
+print 'Please login to your Facebook account.'
+username = raw_input('Email or Phone: ')
+password = getpass()
+
+access_token = 'blahblahblahblahblah'
+
+print 'Your access token is', access_token
