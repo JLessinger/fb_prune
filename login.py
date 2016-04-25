9c6451cbf6360d42445f9b3b1273dc87458ca2cb
Jonathan Lessinger <jonathan.lessinger@gmail.com>
2016-04-25 17:30:49 -0400

    load fb sdk in browser

--- login.py
+++ login.py
@@ -1,39 +1,7 @@
-import execjs
-import requests
+import sys
 
-def load_fb_sdk():
-    response = requests.get('https://connect.facebook.net/en_US/sdk.js')
-    return execjs.compile(response.text)
-
-def callback(response):
-    if response.status == 'connected':
-        # Logged into your app and into Facebook.
-        pass
-    elif response.status == 'not_authorized':
-        # Logged into FB but not your app
-        pass
-    else:
-        # Not logged into Facebook
-        pass
-
-class FBLogin:
-
-    fbsdk = load_fb_sdk()
-
-    def __init__(self):
-        pass
-
-vm = load_fb_sdk()
-
-with open('login.js', 'r') as f:
-    vm.eval(f.read())
-
-authResponse = vm.eval('state')
-
-# Can't pass a function -- TypeError: not JSON serializable
-# vm.call("FB.login", callback)
-
-
-#FB = vm.eval("FB")
-#print FB
-#print FB.login
+if __name__ == '__main__':
+    html = open('sdk.html', 'r').read()
+    new_html = html % sys.argv[1]
+    with open(sys.argv[2], 'w') as html_file:
+        html_file.write(new_html)
